8c93152ecfc9831be0c933e7ce01ead70ebe7838
Aldhaneka <aldhanekadev@gmail.com>
2020-11-15 15:48:41 +0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,38 @@
 var express = require('express');
 var app = express();
 
+console.log("Hello World");
+console.log(__dirname);
+app.use(express.static(__dirname + "/public"))
+
+// send file
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+})
+
+// a simple API
+app.get("/json", (req, res) => {
+  res.json({"message": "Hello json"});
+})
+
+// the first solution
+app.get("/", (req, res) => {
+    res.send("Hello Express");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
